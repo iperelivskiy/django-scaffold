27603371ece999e169682033264566606bd67f03
Ivan Perelivskiy <livskiy@gmail.com>
2012-08-27 13:25:31 +0600

    grappelli urls

--- src/project_name/project_name/urls.py
+++ src/project_name/project_name/urls.py
@@ -9,5 +9,6 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
 	url(r'^$', TemplateView.as_view(template_name='home.html'), name='home'),
+	url(r'^grappelli/', include('grappelli.urls')),
 	url(r'^admin/', include(admin.site.urls)),
 )
